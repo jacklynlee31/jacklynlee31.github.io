a670d2cc7b82ec844c14204747b52b496fdd5bd0
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-09-10 12:08:26 -0500

    Starting out, already added some styling and text.

---
+++ assets/js/custom.js
@@ -0,0 +1,33 @@
+﻿
+
+/*=============================================================
+    Authour URL: www.designbootstrap.com
+
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */
+
+
+$(document).ready(function () {
+
+    // FANCY BOX FOR POPUP
+
+    $('.fancybox-media').fancybox({
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+        helpers: {
+            title: {
+                type: 'inside'
+            }
+        }
+    });
+
+});
